0c897875cde27f32f0b50eac7d2e74ae73fd024a
chernistry <sanderchernitsky@gmail.com>
2026-05-10 14:31:35 +0300

    wip: pre-pause snapshot for #1222

--- packages/vscode/jest.config.js
+++ packages/vscode/jest.config.js
@@ -1,8 +1,9 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['**/src/__tests__/**/*.test.ts'],
-  moduleNameMapper: {
-    vscode: '<rootDir>/src/__mocks__/vscode.ts',
-  },
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/src/__tests__/**/*.test.ts'],
+  moduleNameMapper: {
+    vscode: '<rootDir>/src/__mocks__/vscode.ts',
+  },
 };
+
